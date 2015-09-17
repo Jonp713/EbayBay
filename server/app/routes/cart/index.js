bc889d6528fcefd89084c0bf57d283903411a2dd
Chandra Galbiati <chandragalbiati@Christophers-MacBook-Air.local>
2015-09-17 12:02:34 -0400

    cart is the worst

--- server/app/routes/cart/index.js
+++ server/app/routes/cart/index.js
@@ -3,6 +3,7 @@ var router = require('express').Router();
 module.exports = router;
 var mongoose = require('mongoose');
 var User = mongoose.model('User');
+var R = require('ramda');
 
 
 router.get('/', function(req, res, next) {
@@ -12,8 +13,25 @@ router.get('/', function(req, res, next) {
 router.post('/', function(req, res, next) {
     console.log(req.body);
     console.log('req.session', req.session);
+    //make sure there is a cart on the session
     if(!req.session.cart) req.session.cart = [];
-    req.session.cart.push(req.body.product);
-    if(req.user) req.user.addToCart(req.body.product);
-    res.sendStatus(201);
+
+    //if the product is in the cart add the quantities
+    var findIndexInCart = R.findIndex(R.propEq('_id', req.body.product._id));
+    var index = findIndexInCart(req.session.cart);
+    if(index + 1) req.session.cart[index].quantity += req.body.product.quantity;
+    else {
+		req.session.cart.push(req.body.product);
+    } 
+
+    /// FIND USER IN DB, user.cart = req.session.cart
+    ///deal with product quantities - changing in cart changes in db
+
+    if(req.user) req.user.addToCart(req.body.product)
+    .then(function(){
+    	res.sendStatus(201);
+    });
+
+//update route - decrement
+
 });
