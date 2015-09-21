000a01fbd11c82610c25cb0952a30e691b0620d6
Kate <katemcdonald39@gmail.com>
2015-09-20 22:04:46 -0400

    started adding backbone for google O-Auth. Still in progress.

--- server/env/development.js
+++ server/env/development.js
@@ -1,6 +1,6 @@
 module.exports = {
   "DATABASE_URI": "mongodb://localhost:27017/stackstore-app",
-  "SESSION_SECRET": "Optimus Prime is my real dad",
+  "SESSION_SECRET": "Optimus Prime is my grandpa",
   "TWITTER": {
     "consumerKey": "INSERT_TWITTER_CONSUMER_KEY_HERE",
     "consumerSecret": "INSERT_TWITTER_CONSUMER_SECRET_HERE",
@@ -12,8 +12,11 @@ module.exports = {
     "callbackURL": "INSERT_FACEBOOK_CALLBACK_HERE"
   },
   "GOOGLE": {
-    "clientID": "INSERT_GOOGLE_CLIENTID_HERE",
-    "clientSecret": "INSERT_GOOGLE_CLIENT_SECRET_HERE",
-    "callbackURL": "INSERT_GOOGLE_CALLBACK_HERE"
+    "clientID": "43115196891-hk2oio696aov7o5d39p9sv8ftec7sgec.apps.googleusercontent.com",
+    "clientSecret": "nG85XkIDrSApHUeFDT1U9kY2",
+    "callbackURL": "http://127.0.0.1:1337/auth/google/callback"
+  },
+  "MANDRILL": {
+    "key": "2Ro09IFqZGQ4zXfrAeC0Gw"
   }
 };
