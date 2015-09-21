7e0b13a49b7eb819ca8a9bc1fe29f1e9251399ef
Jon Pedigo <jrp12@hampshire.edu>
2015-09-21 12:04:00 -0400

    addreview working

--- browser/js/add-review/add-review.js
+++ browser/js/add-review/add-review.js
@@ -1,26 +1,32 @@
 app.config(function ($stateProvider) {
 
     $stateProvider.state('addReview', {
-        url: '/review/add',
+        url: '/review/add/:userId',
         templateUrl: 'js/add-review/add-review.html',
         controller: 'AddReviewCtrl',
         // resolve: {
-        //     cartItems: function(CartFactory){
-        //         return CartFactory.getCart();
-        //     }
-        // }
+  //       	aboutUser: function($stateParams){
+  //       		return UserFactory.find($stateParams.id);
+  // 			};
+  //       }
     });
 
 });
 
-app.controller('AddReviewCtrl', function ($scope, CartFactory, $state, cartItems) {
-    // $scope.updateCart = false;
-    // $scope.cartItems = cartItems;
-    // $scope.runUpdate = function() {
-    //     CartFactory.updateCart($scope.cartItems)
-    //     .then(function(response) {
-    //             console.log(response);
-    //             $scope.cartItems = response;
-    //         });
-    // };
+app.controller('AddReviewCtrl', function ($scope, $state, ReviewFactory, AuthService, $stateParams) {
+    
+	$scope.addReview = function(review){
+		
+		AuthService.getLoggedInUser().then(function(user){
+			
+			review.byUser = user._id;
+			review.aboutUser = $stateParams.userId;
+			
+			return ReviewFactory.create(review);
+			
+		}).then(function(element){
+			console.log(element);
+            $state.go('user', {id: element.aboutUser});
+		});
+	}
 });
